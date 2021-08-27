57d2cace1ea692dfd95b7d6496bd0e57c2c9892b
sbaoz <xiaojz821@hotmail.com>
2021-08-27 23:11:21 +0800

    update

--- interview/Frame_knowledge/React/demo/src/pages/React进阶实践指南Demo/10/强化props.jsx
+++ interview/Frame_knowledge/React/demo/src/pages/React进阶实践指南Demo/10/强化props.jsx
@@ -1,49 +1,62 @@
-import React from 'react';
-import { RouterContext } from 'react-router-dom';
-import hoistStatics from 'hoist-non-react-statics'
+import React from "react";
+import hoistStatics from "hoist-non-react-statics";
 
+const ThemeContext = React.createContext();
 class Index extends React.Component {
-    render() {
-        console.log(this);
-
-        return (
-            <div>hello, world</div>
-        )
-    }
+  static displayName = "Index";
+  static staticXXX = "xxx";
+  render() {
+    const { content, color } = this.props;
+    return <div style={{color}}>{content}</div>;
+  }
 }
 
-function WithRouter(Component) {
-    const displayName = `withRouter(${Component.displayName})`;
-    const C = props => {
-        const {wrappedComponentRef, ...remainingProps} = props;
-        return (
-            <RouterContext.Consumer>
-                {
-                    context => {
-                        return (
-                            <Component 
-                                {...remainingProps}
-                                {...context}
-                                ref={wrappedComponentRef}
-                            />
-                        );
-                    }
+function WithTheme(Component) {
+  const displayName = `withRouter(${Component.displayName})`;
+  const C = (props) => {
+    const { wrappedComponentRef, ...remainingProps } = props;
+    return (
+        <ThemeContext.Consumer>
+            {
+                context => {
+                    return (
+                        <Component 
+                            {...context}
+                            {...remainingProps} 
+                            ref={wrappedComponentRef} 
+                        />
+                    )
                 }
-            </RouterContext.Consumer>
-        );
-    };
-    C.displayName = displayName;
-    C.WrappedComponent = Component;
-    /* 继承静态属性 */
-    return hoistStatics(C, Component);
-};
-
-const WithRouterndex = WithRouter(Index);
-
-function HOCDemo() {
-    React.useEffect(() => {
-    }, []);
-    return <WithRouterndex />
+            }
+        </ThemeContext.Consumer>
+    );
+  };
+  C.displayName = displayName;
+  C.WrappedComponent = Component;
+  /* 继承静态属性 */
+  hoistStatics(C, Component);
+  return React.forwardRef((props, ref) => {
+    return <C wrappedComponentRef={ref} {...props} />;
+  });
 }
 
-export default HOCDemo;
+const WithThemeIndex = WithTheme(Index);
+
+function HOCDemo(props) {
+  const indexRef = React.useRef(null);
+  React.useEffect(() => {
+    console.log(indexRef);
+  }, []);
+  return <WithThemeIndex ref={indexRef} content={props.content} />;
+}
+
+function App() {
+  return (
+    <ThemeContext.Provider value={{color: 'red'}}>
+        <Index content={"hello, world"} />
+        <HOCDemo content={"hello, world"} />
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
